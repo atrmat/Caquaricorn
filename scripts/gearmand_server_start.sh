f44ddb38708486f76394846af22ebbd42c876eaa
atrmat <atrmat@gmail.com>
2013-11-06 16:52:27 +0800

    add some scripts

---
+++ scripts/gearmand_server_start.sh
@@ -0,0 +1,1 @@
+gearmand -d --log-file /var/log/gearmand.log --pid-file /tmp/gearmand.pid
---
+++ scripts/merge_jbc.sh
@@ -0,0 +1,8 @@
+echo "merge temp_n : 0 - 255 \n"
+/root/cx_src/src/prepare_test2 /tmp/data /tmp/result/ino.txt /tmp/result/lno.txt /tmp/result/cdo.txt 4 0 255 
+echo "merge temp_n : 256 - 511 \n"
+/root/cx_src/src/prepare_test2 /tmp/data /tmp/result/ino.txt /tmp/result/lno.txt /tmp/result/cdo.txt 4 256 511 
+echo "merge temp_n : 512 - 767 \n"
+/root/cx_src/src/prepare_test2 /tmp/data /tmp/result/ino.txt /tmp/result/lno.txt /tmp/result/cdo.txt 4 512 767 
+echo "merge temp_n : 768 - 1023 \n"
+/root/cx_src/src/prepare_test2 /tmp/data /tmp/result/ino.txt /tmp/result/lno.txt /tmp/result/cdo.txt 4 768 1023 
