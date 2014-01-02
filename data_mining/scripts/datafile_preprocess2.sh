572b28c950292b0e6081d6d64d8d26db388e291a
atrmat <atrmat@gmail.com>
2014-01-02 15:29:36 +0800

    2014.1.2, 25th

---
+++ data_mining/scripts/datafile_preprocess2.sh
@@ -0,0 +1,50 @@
+#preprocessing the data file before clustering
+# the data file is preprocessed by the java program
+# replace the dot with space, and split it
+#!/bin/bash
+SRC_FILE=
+DST_FILE=
+usage()
+{
+    echo "Usage: `basename $0` [-s] src_file [-d] dst_file"
+    exit 1
+}
+
+[ $# -eq 0 ] && usage
+
+while getopts :s:d: OPTION
+do
+    case $OPTION in
+        s)
+            SRC_FILE=$OPTARG
+            ;;
+        d)
+            DST_FILE=$OPTARG
+            ;;
+        \?)
+            usage
+            ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+if [ -z "$SRC_FILE" ]; then
+    echo "You must specify SRC_FILE with -s option"
+    exit
+fi
+if [ -z "$DST_FILE" ]; then
+    echo "You must specify DST_FILE with -d option"
+    exit
+fi
+cp $SRC_FILE $DST_FILE
+sed -i "s/,/ /g" `grep "," -rl ./$DST_FILE`
+gawk '{print NF}' $DST_FILE | sort -nu | more
+rm -rf datafile;./spilt_big_file.sh -s `pwd`/$DST_FILE -p testData_ -n 10 -t csv
+cd datafile/
+for files in `ls`
+do
+    echo $files
+    gawk '{print NF}' $files | sort -nu | more
+done
+
---
+++ data_mining/test/CSVDataPreprocess.java
@@ -0,0 +1,310 @@
+package atrmat;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * @category util tools
+ * func: preprocess the src csv file
+ * */
+public class CSVDataPreprocess {
+	public static String csvTitle[];
+	
+	/**
+     * ip address to long
+     * @param ip
+     * @return
+     */  
+   public static long ip2long(String ip) {  
+       if (ip.equals("null"))
+    	   return -1;
+	   String[] ips = ip.split("[.]");  
+       long num =   16777216L*Long.parseLong(ips[0]) + 65536L*Long.parseLong(ips[1]) + 256*Long.parseLong(ips[2]) + Long.parseLong(ips[3]);  
+       return num;  
+    }  
+     
+   /**
+     * long to ip address
+     * @param ipLong
+     * @return
+     */  
+   public static String long2ip(long ipLong) {  
+       //long ipLong = 1037591503;  
+       long mask[] = {0x000000FF,0x0000FF00,0x00FF0000,0xFF000000};  
+       long num = 0;  
+        StringBuffer ipInfo = new StringBuffer();  
+       for(int i=0;i<4;i++){  
+            num = (ipLong & mask[i])>>(i*8);  
+           if(i>0) ipInfo.insert(0,".");  
+            ipInfo.insert(0,Long.toString(num,10));  
+        }  
+       return ipInfo.toString();  
+    }  
+   
+	public static void main(String[] args) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(
+					"J://GENERIC_CorrelatedXDR.csv"));// the source file path
+			int linenum = 0;
+			int itemnum = 0;
+			String itemLine = null;
+			String line = null;
+			line = reader.readLine();// first line is the title of csv file
+			linenum++;
+			long start = -1;
+			long end = -1;
+			long sessionTime = 0;
+			long ConnectionTime = 0;
+			File file = new File("./GENERIC_CorrelatedXDR_preprocessed.csv");// output the preprocessed file
+			FileOutputStream out = new FileOutputStream(file);
+			OutputStreamWriter osw = new OutputStreamWriter(out, "UTF8");
+			BufferedWriter bw = new BufferedWriter(osw);
+
+			csvTitle = line.split(",");
+			System.out.println(csvTitle.length);
+
+			for (int i = 0; i <= csvTitle.length - 1; i++) {
+				System.out.print(csvTitle[i] + ",");
+				// write the csv title to the output file
+				// if (i < csvTitle.length -1)
+				// bw.write(csvTitle[i]+",");
+				// else
+				// bw.write(csvTitle[i]+"\n");
+			}
+			while ((line = reader.readLine()) != null) {
+				linenum++;
+				itemnum++;
+				String item[] = line.split(",");// split by dot -- ","
+//				String last = item[item.length - 1];
+				// int value = Integer.parseInt(last);//
+				// System.out.println(last);
+
+				for (int i = 0; i <= item.length - 1; i++) {
+					switch (csvTitle[i]) {
+					case "XDR ID":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine = item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "sessionStartTime":
+						start = new Long(item[i].trim());
+						break;
+					case "sessionEndTime":
+						end = new Long(item[i].trim());
+						if (start != -1) {
+							sessionTime = end - start;
+							end = -1;
+							start = -1;
+						}
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += sessionTime + ",";
+							// bw.write(sessionTime+",");
+						}
+						break;
+					case "ClientAddress":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += ip2long(item[i]) + ",";
+							// bw.write(ip2long(item[i])+",");
+						}
+						break;
+					case "ServerAddress":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += ip2long(item[i]) + ",";
+							// bw.write(ip2long(item[i])+",");
+						}
+						break;
+					case "IpID":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "ClientPort":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "ConnectionEndTime":
+						end = new Long(item[i].trim());
+						break;
+					case "ConnectionStartTime":
+						start = new Long(item[i].trim());
+						if (end != -1) {
+							ConnectionTime = end - start;
+							end = -1;
+							start = -1;
+						}
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += ConnectionTime + ",";
+							// bw.write(ConnectionTime+",");
+						}
+						break;
+					case "ClientTunnelAddress":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += ip2long(item[i]) + ",";
+							// bw.write(ip2long(item[i])+",");
+						}
+						break;
+					case "ServerTunnelAddress":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += ip2long(item[i]) + ",";
+							// bw.write(ip2long(item[i])+",");
+						}
+						break;
+					case "ClientToServerPackets":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "ServerToClientPackets":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "ClientToServerOctets":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "ServerToClientOctets":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "AverageResponseTime":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "TotalResp":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "SessionLastUpdateTime":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "ClientTeid":
+						// contains empty value
+						break;
+					case "ServerTeid":
+						// contains empty value
+						break;
+					case "appId":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "End User Address":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += ip2long(item[i]) + ",";
+							// bw.write(ip2long(item[i])+",");
+						}
+						break;
+					case "MSISDN":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "IMSI":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "IMEI":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + ",";
+							// bw.write(item[i]+",");
+						}
+						break;
+					case "APN":
+						if (!item[i].equals("")) {
+							itemnum++;
+						}
+						if (item[i].equalsIgnoreCase("cmnet")) {
+							itemLine += "1" + ",";
+							// bw.write("1"+",");
+						} else if (item[i].equalsIgnoreCase("cmwap")) {
+							itemLine += "2" + ",";
+							// bw.write("2"+",");
+						} else {
+							itemLine += "0" + ",";
+							// bw.write("0"+",");
+						}
+						break;
+					case "NumOfUpdates":
+						if (!item[i].equals("")) {
+							itemnum++;
+							itemLine += item[i] + "\n";
+							// bw.write(item[i]+"\n");
+						}
+						break;
+					default:
+						break;
+					}
+					if (csvTitle[i].equals("EndPoinType|IpAddress|TeId")
+							|| csvTitle[i].equals("ECGI|RATType|TAC")
+							|| csvTitle[i].equals("BSID")
+							|| csvTitle[i].equals("IMSI|NSAPI"))
+						continue;
+					if (item[i].equals(""))
+						System.out.println(linenum + ": " + csvTitle[i]
+								+ " contains null or invalid value!");
+				}
+				if (itemnum < 23) {
+					System.out.println(linenum + ": "
+							+ "lacks values, item num is " + itemnum);
+				} else {
+					bw.write(itemLine);
+				}
+				itemnum = 0;
+				itemLine = null;
+			}
+            bw.close();
+            osw.close();
+            out.close(); 
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
